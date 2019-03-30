4f67e3424be477b33fd00745fb6e1e291792fed9
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-03-30 16:41:38 +0100

    Added random scientific name generator.

--- ngesh/__init__.py
+++ ngesh/__init__.py
@@ -10,6 +10,6 @@ from ngesh.random_tree import gen_tree, random_labels
 
 # Generates and displays a random tree
 def display_random_tree():
-    tree = gen_tree(1.0, 0.5, max_time=3, labels="human")
+    tree = gen_tree(1.0, 0.5, max_time=3, labels="bio")
     tree.show()
 
