d34807012b31b56e65dc77c44d577affef9c38e5
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-03-30 14:05:56 +0100

    First actual tests

--- tests/test_ngesh.py
+++ tests/test_ngesh.py
@@ -12,9 +12,47 @@ import unittest
 import ngesh
 
 class TestNgesh(unittest.TestCase):
-    def test_a(self):
-        self.assertTrue(0 == 0)
+    """
+    Class for `ngesh` tests.
+    """
+    
+    # We first test generations, just to see if no exception is thrown, etc.
+    # For all tests, we use the same parameters of birth rate as 1.0
+    # and death rate as 0.5.
+    def test_generation_min_leaves(self):
+        """
+        Tests tree generation with minimum leaf number stop criterion.
+        """
+        
+        ngesh.gen_tree(1.0, 0.5, min_leaves=5)
+
+
+    def test_generation_max_time(self):
+        """
+        Tests tree generation with maximum_time stop criterion.
+        """
+        
+        ngesh.gen_tree(1.0, 0.5, max_time=2.5)
+
 
 if __name__ == "__main__":
     import sys
     sys.exit(unittest.main())
+    
+#    for i in range(2):
+#        yuletree = birth_only(1.0, min_leaves=10, human=True)
+#        yuletree = birth_only(1.0, max_time=3)
+
+#    for i in range(2):
+#        print("leaves")
+#        bdtree = mine(1.0, 0.5, min_leaves=10, human=True)
+#        if bdtree:
+#            bdtree.show()
+            
+#        bdtree = gen_tree(1.0, 0.5, max_time=3, human_labels=True)
+#        if bdtree:
+#            bdtree.show()
+
+    #print(dir(yuletree))
+    #print(dir(bdtree))
+    #bdtree.show()
