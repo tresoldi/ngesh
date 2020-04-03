81313f9aef9e3745ec33c996e551ef44fa9525cf
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-04-03 18:08:25 +0200

    Experiment with general numpy random

--- ngesh/__init__.py
+++ ngesh/__init__.py
@@ -5,8 +5,6 @@ __version__ = "0.4"
 __author__ = "Tiago Tresoldi"
 __email__ = "tresoldi@shh.mpg.de"
 
-import random
-
 # Build the namespace
 from ngesh.random_tree import gen_tree, add_characters
 from ngesh.output import tree2nexus, tree2wordlist
@@ -14,9 +12,11 @@ from ngesh.utils import set_seeds, random_labels, random_species
 
 # Generates and displays a random tree
 def display_random_tree():
+    import numpy as np
+
     # We make sure the death rate is less than the birth one.
-    l = random.random()
-    mu = random.random() * l * 0.9
+    l = np.random.random()
+    mu = np.random.random() * l * 0.9
 
     tree = gen_tree(l, mu, max_time=4.0, labels="human")
     tree.show()
