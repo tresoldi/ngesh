f43c715ed2125cf46521d380df0d420afc2ee641
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-03-07 14:10:49 +0100

    Fix import with bad refactor

--- src/ngesh/__init__.py
+++ src/ngesh/__init__.py
@@ -10,5 +10,5 @@ __author__ = "Tiago Tresoldi"
 __email__ = "tresoldi@shh.mpg.de"
 
 # Build the namespace
-from src.ngesh.output import tree2nexus, tree2wordlist
+from .output import tree2nexus, tree2wordlist
 from .random_tree import gen_tree, add_characters, simulate_bad_sampling
