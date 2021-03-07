61cf4103a5223e8184f40a30d9805e6b3446e257
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-03-08 00:24:11 +0100

    More test improvements

--- tests/test_characters.py
+++ tests/test_characters.py
@@ -2,35 +2,6 @@ import hashlib
 from ete3 import Tree
 import ngesh
 
-# Some pre-generated newick trees for testing
-_TREES = [
-    "(((Nucroto zolos:0.339415,Coddopus zoggaus:0.339415)1:2.29301,Aporos "
-    "oiasis:2.63243)1:0.511706,Spetitis mubvoppis:3.14413);",
-    "((((Ataba eolus:0.274414,Dasoros audus:0.274414)1:1.59309,(Uvuros "
-    "spalus:1.63679,(Zilavis sicagas:0.158265,Uzazopus aolo:0.158265)"
-    "1:1.47853)1:0.230708)1:0.976915,Spempo gipus:2.84442)1:1.08647,"
-    "(((Cobbas linis:0.242355,Ciggus sopebbas:0.242355)1:1.33741,"
-    "(Vaoras ovamla:0.235349,Nirceo spemgazzo:0.235349)1:1.34442)"
-    "1:0.796904,(Wiopepus spiparzas:1.86067,Eavoros airos:0.906654)"
-    "1:0.515998)1:1.55422);",
-    "((((Egasis reggasas:0.0747242,(Niponis ecales:0.381168,Vossignis "
-    "spepupapes:0.381168)1:0.0385491)1:0.697337,Tuapebbos "
-    "eppozas:0.457931)1:2.02569,(Vucotes sparas:3.09293,((Ruttinges "
-    "daerus:0.300024,(Ovugas setlanes:0.297199,Rezas "
-    "emdis:0.260688)1:1.20787)1:1.35227,(Gemdas naoris:1.77171,(Tirreres "
-    "sbissas:0.161549,Speppizipus spebbes:0.33078)1:1.44093)1:1.08563)"
-    "1:0.235588)1:0.0498126)1:0.593566,Seti spines:0.874473);",
-    "(((Uorus sbepis:1.00768,Winozbus veses:1.00768)1:2.02024,(((((Ragis "
-    "zubarivos:0.239467,(Zoprotollas naessadda:0.032829,Sauvo "
-    "tibbunnis:0.032829)1:0.206638)1:0.298691,Mirba "
-    "ummes:0.538158)1:0.751761,Gitopes guaras:1.28992)1:0.219143,(Uvoros "
-    "sbego:0.429929,Rouslis novuddenus:0.429929)1:1.07913)1:0.0145254,"
-    "(((Spaupis gieves:0.155583,Ruasus coeddas:0.155583)1:0.222901,Tipapo "
-    "vati:0.378484)1:0.044333,Zuvcus luruzuco:0.422817)1:1.10077)"
-    "1:1.50433)1:0.97142,((Nunpis ozabmis:0.491355,Sbigirrecas sbilpo:"
-    "0.491355)1:0.33275,Agolus galgis:0.824105)1:3.17523);",
-]
-
 
 def test_add_characters():
     # gamma parameters
@@ -38,26 +9,56 @@ def test_add_characters():
     k = 4.0  # shape
     th = 1.0  # scale
     z = 1.045  # "zipf" correction
-    z = 1.0  # TODO: fix test later
+
+    # Create trees, also for coverage/profile
+    _trees = [
+        ngesh.gen_tree(
+            1.0, 0.5, max_time=3.0, labels="bio", method="standard", seed=1234
+        ),
+        ngesh.gen_tree(1.0, 0.5, min_leaves=25, lam=2.5, method="fast", seed="myseed"),
+        ngesh.gen_tree(1.1, 0.0, min_leaves=7, lam=1.5, method="fast", seed="uppsala"),
+    ]
 
     # Add characters to all trees, for coverage
     trees = [
-        ngesh.add_characters(
-            Tree(newick), NUM_CONCEPTS, k=k, th=th, mut_exp=z, seed="myseed"
-        )
-        for newick in _TREES
+        ngesh.add_characters(tree, NUM_CONCEPTS, k=k, th=th, mut_exp=z, seed="myseed")
+        for tree in _trees
     ]
 
-    # Assert the first one
-    digest = hashlib.sha256(str(ngesh.tree2wordlist(trees[0])).encode("utf-8")).digest()
+    # Assert the trees by digest
+    digest0 = hashlib.sha256(
+        str(ngesh.tree2wordlist(trees[0])).encode("utf-8")
+    ).digest()
+    digest1 = hashlib.sha256(
+        str(ngesh.tree2wordlist(trees[1])).encode("utf-8")
+    ).digest()
+    digest2 = hashlib.sha256(
+        str(ngesh.tree2wordlist(trees[2])).encode("utf-8")
+    ).digest()
+
     assert (
-        digest
-        == b"\xdc<\x1f\x10N\xbf\xcc\xc4|l26\x10\xfc\xbaN\xb7\\c\x8bB\xca\x95.\xcbH\x82T\xa3\xbd\xff\x15"
+        digest0
+        == b"9\xbf\x8e\x1a,n\x19\x9c\xb6Z\xa3\x9b\x98\x99\x08\xe6\xe6\x94\xb0]\xa00\xd6s\xe3\x03\xc5\xad\x8a\xfc_\x98"
+    )
+
+    assert (
+        digest1
+        == b"\xd7\xa6'9+dni\xbc\xa6s\xa0\x81\xdf\xcb\x00K$\xf2(\xdc\xf0K\\\x1e/\xaa\x1d\xd7gQ\x18"
+    )
+
+    assert (
+        digest2
+        == b"\xda\xbe;B\x04\xdf\xde\xdd\x88\xd3\x95\xbcL\xca\xe8\xc7'\xde\xd6G@\x03\x16\t\xc1\xe4\x07\x90C\x00M{"
     )
 
 
 def test_add_characters_with_hgt():
-    tree_hgt = Tree(_TREES[0])
+
+    # Create tree
+    tree_hgt = ngesh.gen_tree(
+        1.0, 0.5, min_leaves=15, lam=2.5, method="fast", seed="myseed"
+    )
+
     ngesh.add_characters(
         tree_hgt,
         10,
@@ -72,5 +73,5 @@ def test_add_characters_with_hgt():
 
     assert (
         digest
-        == b"\xc0\x84\x8a\xa8\x0b\xd6\xf8\x1b\x9c\xc7\xe6\xaf]\xe7\xee\x105\x989\x9b\xcfke\xe5\xf3\x03\xdc\x17U\x14 \xac"
+        == b"@\x1c\xf7\xc7\xe1]]\x1e\x10\xd8\xd8<\x8aH\xaa\x1b\xdb\xd6\xfb:\xbbD\xea\x053\x10\x96\x85(@y\xeb"
     )
