3a97b77f5af99a57ba492c66ce6b704006ab7009
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-03-07 12:42:58 +0100

    Prepare repository for GitHub actions

--- AUTHORS.md
+++ AUTHORS.md
@@ -5,3 +5,8 @@ Development
 -----------
 
 * Tiago Tresoldi <tresoldi@shh.mpg.de>
+
+Contribution
+------------
+
+* Nicola de Maio <demaio@ebi.ac.uk> -- "fast" method for computation of random trees
--- setup.py
+++ setup.py
@@ -1,41 +1,48 @@
+"""
+setup.py for the `ngesh` library
+
+Requirements are listed in `requirements.txt`.
+"""
+
 import pathlib
+
+# Import Python standard libraries
 from setuptools import setup
 
 # The directory containing this file
 LOCAL_PATH = pathlib.Path(__file__).parent
 
 # The text of the README file
-README_FILE = (LOCAL_PATH / "README.md").read_text()
-
+README_FILE = (LOCAL_PATH / "README.md").read_text(encoding="utf-8")
 
 # Load requirements, so they are listed in a single place
-with open("requirements.txt") as fp:
+with open("requirements.txt", encoding="utf-8") as fp:
     install_requires = [dep.strip() for dep in fp.readlines()]
 
 # This call to setup() does all the work
 setup(
-    name="ngesh",
-    version="0.4.1",
-    description="Simulate random phylogenetic trees",
-    long_description=README_FILE,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tresoldi/ngesh",
     author="Tiago Tresoldi",
-    author_email="tresoldi@shh.mpg.de",
-    license="MIT",
+    author_email="tiago.tresoldi@lingfil.uu.se",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries",
     ],
-    packages=["ngesh"],
-    python_requires=">=3.7",
-    keywords=["random phylogenetic tree", "phylogenetics", "simulation"],
+    description="Simulate random phylogenetic trees",
+    entry_points={"console_scripts": ["ngesh=ngesh.__main__:main"]},
     include_package_data=True,
     install_requires=install_requires,
-    entry_points={"console_scripts": ["ngesh=ngesh.__main__:main"]},
+    keywords=["random phylogenetic tree", "phylogenetics", "simulation"],
+    license="MIT",
+    long_description=README_FILE,
+    long_description_content_type="text/markdown",
+    name="ngesh",
+    packages=["ngesh"],
+    python_requires=">=3.6",
     test_suite="tests",
     tests_require=[],
+    url="https://github.com/tresoldi/ngesh",
+    version="0.4.1",  # remember to sync with __init__.py
     zip_safe=False,
 )
