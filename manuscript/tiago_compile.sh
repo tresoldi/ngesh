cedda3dea50d2e4762ac62bb5a8425af23e0090c
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-03-12 13:37:56 +0100

    Update manuscript (and .gitignote for latex files)

--- manuscript/tiago_compile.sh
+++ manuscript/tiago_compile.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-pandoc \
-    -V graphics="true" \
-    -V geometry:margin=1in \
-    --pdf-engine=xelatex \
-    --bibliography=paper.bib \
-    --csl=apa.csl \
-    --template latex.template \
-    --filter pandoc-citeproc \
-    -o paper.tex \
-    paper.md
-xelatex paper.tex
-xelatex paper.tex
+pandoc -V graphics="true" -V geometry:margin=1in --pdf-engine=xelatex --bibliography=paper.bib --csl=apa.csl --template latex.template --filter pandoc-citeproc -o paper.tex paper.md
+latexmk -xelatex paper.tex
 
